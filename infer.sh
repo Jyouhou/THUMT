cf62aa47c13d57542f9b56deba96546d0f9b4091
JiaqiangRuan <randyruan96@gmail.com>
2018-03-26 21:07:27 +0800

    add sh

---
+++ infer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export PYTHONPATH=/home/rjq/project/THUMT_ruan:$PYTHONPATH
+
+DATA=~/project/nist_thulac
+TRAIN=$DATA/train/
+VALID=$DATA/dev_test/nist06/
+
+
+python /home/rjq/project/THUMT_ruan/thumt/bin/translator.py \
+       --model RNNsearch \
+       --input ${VALID}nist06.cn \
+       --output infer/nist06.en.trans \
+       --checkpoints train/eval --vocabulary ${TRAIN}vocab.zh.32k.txt ${TRAIN}vocab.en.32k.txt \
+       --parameters=device_list=[5]
---
+++ multi-bleu.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+DATA=~/project/nist_thulac
+VALID=$DATA/dev_test/nist06/
+
+perl /home/rjq/bin/multi-bleu.perl ${VALID}nist06.en0 ${VALID}nist06.en1 ${VALID}nist06.en2 ${VALID}nist06.en3 < infer/nist06.en.trans
