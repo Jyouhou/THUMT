9ce718a2abad3c9c94c972d174531012e71b5ac3
JiaqiangRuan <randyruan96@gmail.com>
2018-03-27 12:36:06 +0800

    add multibleu

--- train.sh
+++ train.sh
@@ -5,9 +5,10 @@ TRAIN=$DATA/train/
 VALID=$DATA/dev_test/nist06/
 
 python /home/rjq/project/THUMT_theano_ruan/scripts/trainer.py \
-       --config-file /home/rjq/project/THUMT_theano_ruan/config/THUMT.config \
+#       --config-file /home/rjq/project/THUMT_theano_ruan/config/THUMT.config \
+       --config-file THUMT.config \
        --trn-src-file ${TRAIN}train.zh.shuf \
        --trn-trg-file ${TRAIN}train.en.shuf \
        --vld-src-file ${VALID}nist06.cn \
        --vld-trg-file ${VALID}nist06.en0 \
-       --device {gpu3, gpu4}
+       --device gpu3,gpu4
