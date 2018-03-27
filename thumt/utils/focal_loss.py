f0884a43222317cacfe065ce744aef31a25e1419
JiaqiangRuan <randyruan96@gmail.com>
2018-03-27 23:44:04 +0800

    add focal

--- thumt/utils/focal_loss.py
+++ thumt/utils/focal_loss.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def focal_loss(logits, labels, weights=None, alpha=0.25, gamma=2):
+def focal_loss(logits, labels, weights=None, alpha=0.25, gamma=2, scope=None):
     r"""Compute focal loss for predictions.
         Multi-labels Focal loss formula:
             FL = -alpha * (z-p)^gamma * log(p) -(1-alpha) * p^gamma * log(1-p)
@@ -18,20 +18,23 @@ def focal_loss(logits, labels, weights=None, alpha=0.25, gamma=2):
         # loss: A (scalar) tensor representing the value of the loss function
         loss: A [batch_size, num_anchors] tensor representing the value of the loss function
     """
-    sigmoid_p = tf.nn.sigmoid(logits)
-    zeros = tf.zeros_like(sigmoid_p, dtype=sigmoid_p.dtype)
-    vocab_size = tf.shape(logits)[1]
+    with tf.name_scope(scope or "smoothed_softmax_cross_entropy_with_logits",
+                       values=[logits, labels]):
 
-    labels = tf.reshape(labels, [-1])
-    labels = tf.one_hot(tf.cast(labels, tf.int32), depth=vocab_size)
-    # For poitive prediction, only need consider front part loss, back part is 0;
-    # target_tensor > zeros <=> z=1, so poitive coefficient = z - p.
-    pos_p_sub = tf.where(labels > zeros, labels - sigmoid_p, zeros)
+        sigmoid_p = tf.nn.sigmoid(logits)
+        zeros = tf.zeros_like(sigmoid_p, dtype=sigmoid_p.dtype)
+        vocab_size = tf.shape(logits)[1]
 
-    # For negative prediction, only need consider back part loss, front part is 0;
-    # target_tensor > zeros <=> z=1, so negative coefficient = 0.
-    neg_p_sub = tf.where(labels > zeros, zeros, sigmoid_p)
-    per_entry_cross_ent = - alpha * (pos_p_sub ** gamma) * tf.log(tf.clip_by_value(sigmoid_p, 1e-8, 1.0)) \
+        labels = tf.reshape(labels, [-1])
+        labels = tf.one_hot(tf.cast(labels, tf.int32), depth=vocab_size)
+        # For poitive prediction, only need consider front part loss, back part is 0;
+        # target_tensor > zeros <=> z=1, so poitive coefficient = z - p.
+        pos_p_sub = tf.where(labels > zeros, labels - sigmoid_p, zeros)
+
+        # For negative prediction, only need consider back part loss, front part is 0;
+        # target_tensor > zeros <=> z=1, so negative coefficient = 0.
+        neg_p_sub = tf.where(labels > zeros, zeros, sigmoid_p)
+        per_entry_cross_ent = - alpha * (pos_p_sub ** gamma) * tf.log(tf.clip_by_value(sigmoid_p, 1e-8, 1.0)) \
                           - (1 - alpha) * (neg_p_sub ** gamma) * tf.log(tf.clip_by_value(1.0 - sigmoid_p, 1e-8, 1.0))
     return tf.reduce_mean(per_entry_cross_ent,-1)
 
