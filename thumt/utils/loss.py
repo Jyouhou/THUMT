59681314cde9497b15db1be19381038b44110a2e
JiaqiangRuan <randyruan96@gmail.com>
2018-03-27 22:21:37 +0800

    add mrt

--- thumt/utils/loss.py
+++ thumt/utils/loss.py
@@ -6,7 +6,7 @@ import thumt.utils.mrt_utils as mrt_utils
 
 
 def get_loss(features, params, ce, tgt_mask):
-    if params.use_mrt:
+    if params.MRT:
         loss = mrt_utils.mrt_loss(features, params, ce, tgt_mask)
     else:
         loss = tf.reduce_sum(ce * tgt_mask) / tf.reduce_sum(tgt_mask)
