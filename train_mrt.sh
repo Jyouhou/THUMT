29b4b23dae42c464f42d7637ce5a304fbe3347e6
JiaqiangRuan <randyruan96@gmail.com>
2018-04-02 20:35:24 +0800

    chage train.sh

---
+++ train_mrt.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DATA=~/project/nist_thulac
+TRAIN=$DATA/train/
+VALID=$DATA/dev_test/nist06/
+
+python /home/rjq/project/THUMT_theano_ruan/scripts/trainer.py \
+       --config-file /home/rjq/project/THUMT_theano_ruan/config/THUMT.config \
+       --config-file THUMT.config \
+       --trn-src-file ${TRAIN}train.zh.shuf \
+       --trn-trg-file ${TRAIN}train.en.shuf \
+       --vld-src-file ${VALID}nist06.cn \
+       --vld-trg-file ${VALID}nist06.en0 \
+       --debug 1 \
+       --training-criterion 1 \
+       --init-model-file model_mle_best.npz \
+       --device gpu3 \
