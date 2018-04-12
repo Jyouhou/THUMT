15e72b5bc1060be80820fba8294832074684c1f4
JiaqiangRuan <randyruan96@gmail.com>
2018-04-12 19:01:37 +0800

    modify mrt

--- infer.sh
+++ infer.sh
@@ -1,16 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 export PYTHONPATH=/home/rjq/project/THUMT_ruan:$PYTHONPATH
 
+#set -ex
 DATA=~/project/nist_thulac
 TRAIN=$DATA/train/
 VALID=$DATA/dev_test/nist06/
 TEST=/home/rjq/project/nist_thulac/dev_test/nist02/
+BLEU=/home/rjq/bin/multi-bleu.perl
+REF=$TEST/nist02.en
 
-
-python /home/rjq/project/THUMT_ruan/thumt/bin/translator.py \
+#mkdir infer
+for ((i=0;i<=300000;i++));
+ do
+   MODEL=train/eval/model.ckpt-$i
+   OUT=infer/nist02.en.trans.iter${i}
+   EVAL=infer/nist02.en.trans.eval.iter${i}
+   #echo ${MODEL}.index
+   if [ -f ${MODEL}.index ];then
+    echo good
+    python /home/rjq/project/THUMT_ruan/thumt/bin/translator.py \
        --model RNNsearch \
        --input ${TEST}nist02.cn \
-       --output infer/nist02.en.trans \
-       --checkpoints train/eval --vocabulary ${TRAIN}vocab.zh.32k.txt ${TRAIN}vocab.en.32k.txt \
+       --output ${OUT} \
+       --checkpoints ${MODEL} --vocabulary ${TRAIN}vocab.zh.32k.txt ${TRAIN}vocab.en.32k.txt \
        --parameters=device_list=[5],decode_alpha=1.0
+    perl $BLEU -lc $REF < $OUT > $EVAL
+  fi
+ done
